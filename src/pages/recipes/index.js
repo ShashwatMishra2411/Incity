ccc20bec11cb18227e0859cc01e54eaac7752087
Rovers11 <spandanmukherjeegithub@gmail.com>
2024-08-13 00:51:03 +0530

    chore: Update npm dependency to latest stable version

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -2,18 +2,64 @@ import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { MdSearch, MdOutlineChat } from 'react-icons/md';
 import { FaWindowClose } from 'react-icons/fa';
+import Image from 'next/image';
+import Loader from '@/components/Loader';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+// Create your API key here https://aistudio.google.com/app/apikey
+const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 
 const RecipeChatbot = ({ toggleChat = () => {} }) => {
   // Chat state
   const [chatHistory, setChatHistory] = useState([]);
   const [messageInput, setMessageInput] = useState('');
+  const [file, setFile] = useState(null);
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   // Popup ref
   const chatRef = useRef(null);
 
-  // Function to handle search input
+  const genAI = new GoogleGenerativeAI(API_KEY);
+
+  const fileToGenerativePart = async (file) => {
+    const base64EncodedDataPromise = new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result.split(',')[1]);
+      reader.readAsDataURL(file);
+    });
+    return {
+      inlineData: { data: await base64EncodedDataPromise, mimeType: file.type }
+    };
+  };
+
+  const fetchDataProVision = async () => {
+    if (!file || !prompt) {
+      alert('Please select an image and enter a prompt');
+      return;
+    }
+    setResponse(null);
+    setLoading(true);
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+    try {
+      const imageParts = await fileToGenerativePart(file);
+      const result = await model.generateContent([prompt, imageParts]);
+      const response = await result.response;
+      const text = response.text();
+      setLoading(false);
+      setResponse(text);
+      setPrompt('');
+      // Use the response to fetch possible dishes
+      handleSearch(text); // Update this to match the structure of your generated text
+    } catch (error) {
+      setError(`Oops, an error occurred: ${error}`);
+      console.log(error);
+    }
+  };
+
   const handleSearch = async (searchTerm) => {
     if (searchTerm.trim() === '') return;
 
@@ -31,7 +77,6 @@ const RecipeChatbot = ({ toggleChat = () => {} }) => {
     }
   };
 
-  // Function to handle chat input
   const handleChatInput = async () => {
     const message = messageInput.trim();
     if (message === '') return;
@@ -46,7 +91,6 @@ const RecipeChatbot = ({ toggleChat = () => {} }) => {
     }
   };
 
-  // Function to update chat history
   const updateChatHistory = (message, recipeData = null) => {
     const newHistory = [
       ...chatHistory,
@@ -82,9 +126,49 @@ const RecipeChatbot = ({ toggleChat = () => {} }) => {
     setLoading(false);
   };
 
-  // Handler for input changes
   const handleInput = (e) => {
     setMessageInput(e.target.value);
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
+
+    if (file && allowedTypes.includes(file.type)) {
+      setFile(file);
+    } else {
+      alert('Please select a valid image file');
+      event.target.value = null;
+    }
+  };
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleImageProcessing = async () => {
+    if (!file || !prompt) {
+      alert('Please select an image and enter a prompt');
+      return;
+    }
+    setResponse(null);
+    setLoading(true);
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+    try {
+      const imageParts = await fileToGenerativePart(file);
+      const result = await model.generateContent([prompt, imageParts]);
+      const response = await result.response;
+      const text = response.text();
+      setLoading(false);
+      setResponse(text);
+      setPrompt('');
+      // Use the response to fetch possible dishes
+      handleSearch(text); // Update this to match the structure of your generated text
+    } catch (error) {
+      setError(`Oops, an error occurred: ${error}`);
+      console.log(error);
+    }
   };
 
   return (
@@ -103,25 +187,48 @@ const RecipeChatbot = ({ toggleChat = () => {} }) => {
               {message.parts}
             </div>
           ))}
-          {loading && <div className='text-center'>Loading...</div>}
+          {loading && <Loader />}
           {error && <div className='text-red-500'>{error}</div>}
-        </div>
-        <div className='flex items-center justify-between'>
-          <input
-            disabled={loading}
-            className='w-full border border-gray-300 px-3 py-2 text-gray-700 rounded-md mt-4 focus:outline-none'
-            placeholder='Type your ingredient'
-            onKeyDown={(e) => (e.key === 'Enter' ? handleChatInput() : null)}
-            onChange={handleInput}
-            value={messageInput}
-          />
-          <button
-            className={`bg-blue-500 px-4 py-2 text-white rounded-md shadow-md hover:bg-blue-600 disabled:bg-slate-500 focus:outline-none ml-4`}
-            disabled={messageInput === '' || loading}
-            onClick={() => handleChatInput()}
-          >
-            <MdSearch size={24} />
-          </button>
+          {response && <div className='text-cyan-300'>Response: {response}</div>}
+          <div className='mt-4'>
+            <input
+              className='w-full border border-gray-300 px-3 py-2 text-gray-700 rounded-md focus:outline-none'
+              type='text'
+              placeholder='Enter prompt for image'
+              value={prompt}
+              onChange={handlePromptChange}
+            />
+            <input
+              className='w-full mt-2 border border-gray-300 px-3 py-2 text-gray-700 rounded-md focus:outline-none'
+              type='file'
+              accept='image/*'
+              onChange={handleFileChange}
+            />
+            <button
+              className='bg-green-500 px-4 py-2 text-white rounded-md shadow-md hover:bg-green-600 focus:outline-none mt-4'
+              onClick={handleImageProcessing}
+              disabled={loading}
+            >
+              Process Image
+            </button>
+          </div>
+          <div className='flex items-center justify-between mt-4'>
+            <input
+              disabled={loading}
+              className='w-full border border-gray-300 px-3 py-2 text-gray-700 rounded-md focus:outline-none'
+              placeholder='Type your ingredient'
+              onKeyDown={(e) => (e.key === 'Enter' ? handleChatInput() : null)}
+              onChange={handleInput}
+              value={messageInput}
+            />
+            <button
+              className={`bg-blue-500 px-4 py-2 text-white rounded-md shadow-md hover:bg-blue-600 disabled:bg-slate-500 focus:outline-none ml-4`}
+              disabled={messageInput === '' || loading}
+              onClick={() => handleChatInput()}
+            >
+              <MdSearch size={22} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
